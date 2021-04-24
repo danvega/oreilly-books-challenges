dc70ecd81818b4f5effbc46fe7e98d7a3a74ffc9
Dan Vega <danvega@gmail.com>
2021-04-24 17:53:56 -0400

    adding a new test for the instance variable welcomeMsg

--- src/test/java/com/oreilly/books/HomeControllerTest.java
+++ src/test/java/com/oreilly/books/HomeControllerTest.java
@@ -1,10 +1,14 @@
 package com.oreilly.books;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +31,19 @@ class HomeControllerTest {
     void homeMethodShouldRespondToRootContext() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
+    }
+
+    /**
+     * A field of type String with the name of welcomeMsg should be declared in the controller
+     *
+     * mvn -Dtest=HomeControllerTest#welcomeMsgInstanceVariableShouldBeDeclared test -q
+     */
+    @Test
+    void welcomeMsgInstanceVariableShouldBeDeclared() throws NoSuchFieldException {
+        Field welcomeMsg = controller.getClass().getDeclaredField("welcomeMsg");
+        assertNotNull(welcomeMsg);
+        assertEquals("java.lang.String",welcomeMsg.getType().getName());
+        System.out.println("success");
     }
 
     /**
