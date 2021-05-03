f495a295cf3dc801bb595dfca867a8179c07e3bb
Dan Vega <danvega@gmail.com>
2021-05-03 10:12:37 -0400

    Removing invalid import

--- src/test/java/com/oreilly/books/BookJdbcTest.java
+++ src/test/java/com/oreilly/books/BookJdbcTest.java
@@ -1,27 +1,24 @@
 package com.oreilly.books;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.io.File;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJdbcTest
 public class BookJdbcTest {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    JdbcTemplate jdbcTemplate;
 
-    private final String RESOURCES_PATH = "src/main/resources";
+    final String RESOURCES_PATH = "src/main/resources";
 
     RowMapper<Book> rowMapper = (rs, rowNum) -> {
         Book book = new Book();
@@ -44,6 +41,9 @@ public class BookJdbcTest {
         assertTrue(file.exists());
     }
 
+    /**
+     * Validate that data.sql exists
+     */
     @Test
     public void dataSqlShouldExist() {
         File file = new File(RESOURCES_PATH + "/data.sql");
