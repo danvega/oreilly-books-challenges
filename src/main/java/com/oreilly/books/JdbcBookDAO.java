223bb840ec83fe57b4cde9338b5caa16c47c30d3
Dan Vega <danvega@gmail.com>
2021-05-03 21:03:20 -0400

    updating the methods that the students will fill in with some helpful comments.

--- src/main/java/com/oreilly/books/JdbcBookDAO.java
+++ src/main/java/com/oreilly/books/JdbcBookDAO.java
@@ -51,37 +51,38 @@ public class JdbcBookDAO implements BookDAO {
         return Optional.of(jdbcTemplate.queryForObject(sql,rowMapper,id));
     }
 
+    /**
+     * I will persist a new instance of a book to the database. The database currently does not auto generate a new id. This means
+     * that the incoming request will have the books id included.
+     *
+     * @param book The new book.
+     * @return the new book that was persisted to the database.
+     */
     @Override
     public Book create(Book book) {
-        Map<String,Object> parameters = new HashMap<>();
-        parameters.put("id",book.getId());
-        parameters.put("title", book.getTitle());
-        parameters.put("author",book.getAuthor());
-        parameters.put("publisher",book.getPublisher());
-        parameters.put("release_date",book.getReleaseDate());
-        parameters.put("isbn",book.getIsbn());
-        parameters.put("topic",book.getTopic());
-        insertBook.execute(parameters);
-        return book;
+        return null;
     }
 
+    /**
+     * I will update an existing book in the database.
+     *
+     * @param book The updated book
+     * @param id The id of the book being updated.
+     * @return The updated book.
+     */
     @Override
     public Book update(Book book, int id) {
-        String sql = "update book set title = ?, author = ?, publisher = ?, release_date = ?, isbn = ?, topic =? where id = ?";
-        jdbcTemplate.update(sql,
-                book.getTitle(),
-                book.getAuthor(),
-                book.getPublisher(),
-                book.getReleaseDate(),
-                book.getIsbn(),
-                book.getTopic(),
-                id);
-        return book;
+        return null;
     }
 
+    /**
+     * I will remove a book from the database
+     *
+     * @param id the id of the book being deleted.
+     */
     @Override
     public void delete(int id) {
-        jdbcTemplate.update("delete from book where id = ?",id);
+
     }
 
 }
