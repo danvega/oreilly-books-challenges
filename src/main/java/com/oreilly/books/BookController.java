000ce26af538c14187e2da56f16c713da586d39b
Dan Vega <danvega@gmail.com>
2021-05-10 21:36:43 -0400

    removing unused imports

--- src/main/java/com/oreilly/books/BookController.java
+++ src/main/java/com/oreilly/books/BookController.java
@@ -5,8 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
